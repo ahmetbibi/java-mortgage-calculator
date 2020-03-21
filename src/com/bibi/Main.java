314eff4c997addbb56fda61ff404816c23326d8a
ahmetbibi <ahmetbibi@gmail.com>
2020-03-21 18:37:26 +0100

    constraints are defined

--- src/com/bibi/Main.java
+++ src/com/bibi/Main.java
@@ -9,20 +9,48 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
-        System.out.print("Principal: ");
-        int principal = new Scanner(System.in).nextInt();
-        System.out.print("Annual Interest Rate: ");
-        Float annualInterestRate = new Scanner(System.in).nextFloat();
-        System.out.print("Period (Years): ");
-        Byte periodYears = new Scanner(System.in).nextByte();
+        int principal = 0;
+        Float annualInterestRate = 0.0F;
+        Byte periodYears = 0;
+        Float monthlyInterestRate = 0.0F;
+        int numberOfPayments = 0;
 
-        Float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        Scanner scanner = new Scanner(System.in);
 
-        int numberOfPayments = periodYears * MONTHS_IN_YEAR;
+        while(true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1000000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate > 0 && annualInterestRate <= 30) {
+                monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 and less than 30");
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            periodYears = scanner.nextByte();
+            if (periodYears >= 1 && periodYears <= 30) {
+                numberOfPayments = periodYears * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
 
         Double monthlyPayment = principal
                 *((monthlyInterestRate*(Math.pow((1 + monthlyInterestRate),numberOfPayments)))
                 /(Math.pow((1 + monthlyInterestRate),numberOfPayments) - 1));
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(monthlyPayment));
+
+
     }
 }
